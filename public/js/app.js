43935f1820c70b8a94f0a1d8d088ed791894b223
Siva Puvvada <siva.puvvada@gmail.com>
2019-07-29 11:01:39 +0530

    Setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,13 @@ const weatherForm = document.querySelector("form");
 const searchTerm = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
+
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = searchTerm.value;
 
   messageOne.textContent = "Loading...";
-  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+  fetch(`/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
         messageOne.textContent = data.error;
